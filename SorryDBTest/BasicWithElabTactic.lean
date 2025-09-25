e84a13366504c5ef4a3bebce1611eaadffa606c0
dhyan-aranha <51273141+dhyan-aranha@users.noreply.github.com>
2025-09-25 14:47:30 +0200

    Add BasicWithElabTactic.lean with placeholder theorems

---
+++ SorryDBTest/BasicWithElabTactic.lean
@@ -0,0 +1,14 @@
+import Lean.Elab.Tactic
+
+theorem test_sorry : 1 + 1 = 2 := by
+  sorry
+
+theorem test_sorry2 : 1 + 1 = 2 := by
+  sorry
+
+theorem test_sorry3 : 1 + 4 = 5 := by
+  sorry
+
+
+theorem test_sorry4 : 1 + 4 = 5 := by
+  sorry
