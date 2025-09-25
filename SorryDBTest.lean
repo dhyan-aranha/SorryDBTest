ec52918d8eeec9b97506443775db19aab7147068
dhyan-aranha <51273141+dhyan-aranha@users.noreply.github.com>
2025-09-25 14:47:57 +0200

    Add import for BasicWithElabTactic module

--- SorryDBTest.lean
+++ SorryDBTest.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `SorryDBTest` library.
 -- Import modules here that should be built as part of the library.
 import SorryDBTest.Basic
+import BasicWithElabTactic.lean
